adaa0f2f1ee3106785934e2e9f82ccbd5e38069d
Mamdouh Jaber <15077216+mjaber95@users.noreply.github.com>
2026-06-10 07:57:52 -0400

    added english greeting

--- api/fast.py
+++ api/fast.py
@@ -17,7 +17,8 @@ app.add_middleware(
 def root():
     response = {
         'greeting': 'Servus, griaß di!',    # This is a typical Bavarian greeting ;)
-        'timestamp': datetime.now()
+        'timestamp': datetime.now(),
+        'greeting_english': "English"
     }
 
     return response
